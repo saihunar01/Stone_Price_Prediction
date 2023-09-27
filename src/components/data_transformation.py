480432aa2b9390d54a4575963dd60d04a3a48a70
saihunar01 <124872291+saihunar01@users.noreply.github.com>
2023-09-27 22:54:38 +0530

    renamed:    notebook/EDA.ipynb -> notebooks/EDA.ipynb 	renamed:    notebook/Model Training.ipynb -> notebooks/Model Training.ipynb 	renamed:    notebook/abc.txt -> notebooks/abc.txt 	renamed:    notebook/data/gemstone.csv -> notebooks/data/gemstone.csv 	modified:   src/components/data_ingestion.py 	deleted:    src/components/data_transform.py 	new file:   src/components/data_transformation.py 	modified:   src/exception.py

---
+++ src/components/data_transformation.py
@@ -0,0 +1,71 @@
+import os
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,OneHotEncoder
+
+from src.logger import logging
+from src.exception import CustomException
+
+
+@dataclass
+class DataTransformationConfig():
+    preprocessor_obj_file_path=os.path.join('artifacts','prepocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+
+        try:
+            logging.info('Data Transformation initiated')
+
+            # Define which columns should be ordinal-encoded and which should be scaled
+            categorical_cols = X.select_dtypes(include='object').columns
+            numerical_cols = X.select_dtypes(exclude='object').columns
+            
+            # Define the custom ranking for each ordinal variable
+            cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
+            color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
+            clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
+            
+            ## Numerical Pipeline
+            num_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='median')),
+                ('scaler',StandardScaler())
+
+                ]
+
+            )
+
+            # Categorigal Pipeline
+            cat_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ('ordinalencoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories])),
+                ('scaler',StandardScaler())
+                ]
+
+            )
+
+            preprocessor=ColumnTransformer([
+            ('num_pipeline',num_pipeline,numerical_cols),
+            ('cat_pipeline',cat_pipeline,categorical_cols)
+            ])
+
+            return preprocessor
+
+            logging.info('Pipeline completed')
+
+            
+
+        except Exception as e:
+            logging.info('Exception occured in data Transformation')
+            raise ChildProcessError(e.sys)
